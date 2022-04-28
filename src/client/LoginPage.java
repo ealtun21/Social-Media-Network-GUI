342abf5b6d944b6cc06f1d3ac903f41633a43a61
Eray Altun <ealtun21@ku.edu.tr>
2022-04-28 16:26:06 +0300

    LoginPage Outline

---
+++ src/client/LoginPage.java
@@ -0,0 +1,13 @@
+package client;
+
+import javax.swing.JFrame;
+
+public class LoginPage extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public LoginPage() {
+		
+	}
+	
+}
--- src/client/test.java
+++ src/client/test.java
@@ -1,7 +1,14 @@
 package client;
 
+import javax.swing.JFrame;
+
 public class test {
 	public static void main(String[] args) {
+		
+		LoginPage login = new LoginPage();
+		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		login.setSize(350, 350);
+		login.setVisible(true);
 		
 	}
 
